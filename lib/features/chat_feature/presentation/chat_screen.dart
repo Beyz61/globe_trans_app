b9e43a9e1b155a1069c3a15d91c19957dfa348e9
dennis <info.dennis.durmus@gmail.com>
2025-01-23 11:01:55 +0100

    feat: add isRead property to Message and display chat start date in ChatScreen

--- lib/features/chat_feature/presentation/chat_screen.dart
+++ lib/features/chat_feature/presentation/chat_screen.dart
@@ -2,7 +2,6 @@ import 'dart:io';
 
 import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
-import 'package:globe_trans_app/features/chat_feature/widget/message_bubble.dart';
 import 'package:globe_trans_app/features/shared/database_repository.dart';
 import 'package:provider/provider.dart';
 
@@ -10,8 +9,9 @@ class Message {
   final String text;
   final bool isSent;
   final DateTime timestamp;
+  final bool isRead; // Add this property
 
-  Message(this.text, this.isSent, this.timestamp);
+  Message(this.text, this.isSent, this.timestamp, {this.isRead = false});
 }
 
 class Chat {
@@ -34,18 +34,18 @@ class ChatScreen extends StatefulWidget {
 
 class _ChatScreenState extends State<ChatScreen> {
   final List<Message> messages = [
-    Message(
-        "Hello, how are you?\n_ _ _ _ _ _ _ _ _ _ _ _ _ _ _ _ _\nWie geht e dir?",
-        false,
-        DateTime.now().subtract(const Duration(minutes: 5))),
-    Message("Mir geht's gut, danke!", true,
-        DateTime.now().subtract(const Duration(minutes: 4))),
-    Message(
-        "Do you have time today?\n_ _ _ _ _ _ _ _ _ _ _ _ _ _ _ _ _ _ _\nHast du heute Zeit?",
-        false,
-        DateTime.now().subtract(const Duration(minutes: 3))),
-    Message("Ja, gerne! Lass uns treffen.", true,
-        DateTime.now().subtract(const Duration(minutes: 2))),
+    // Message(
+    //     "Hello, how are you?\n_ _ _ _ _ _ _ _ _ _ _ _ _ _ _ _ _\nWie geht e dir?",
+    //     false,
+    //     DateTime.now().subtract(const Duration(minutes: 5))),
+    // Message("Mir geht's gut, danke!", true,
+    //     DateTime.now().subtract(const Duration(minutes: 4))),
+    // Message(
+    //     "Do you have time today?\n_ _ _ _ _ _ _ _ _ _ _ _ _ _ _ _ _ _ _\nHast du heute Zeit?",
+    //     false,
+    //     DateTime.now().subtract(const Duration(minutes: 3))),
+    // Message("Ja, gerne! Lass uns treffen.", true,
+    //     DateTime.now().subtract(const Duration(minutes: 2))),
   ];
 
   final TextEditingController _controller = TextEditingController();
@@ -97,10 +97,103 @@ class _ChatScreenState extends State<ChatScreen> {
                           : const CupertinoActivityIndicator());
                 }
                 return ListView.builder(
-                  itemCount: messages.length,
+                  itemCount: messages.length + 1,
                   itemBuilder: (context, index) {
-                    final message = messages[index];
-                    return MessageBubble(message: message);
+                    if (index == 0) {
+                      final now = DateTime.now();
+                      final chatStartDate =
+                          messages.isNotEmpty ? messages.first.timestamp : now;
+                      final isToday = now.day == chatStartDate.day &&
+                          now.month == chatStartDate.month &&
+                          now.year == chatStartDate.year;
+                      final startDateText = isToday
+                          ? "Heute"
+                          : "${_getWeekday(chatStartDate.weekday)} ${chatStartDate.day}.${_getMonth(chatStartDate.month)}";
+                      return Padding(
+                        padding: const EdgeInsets.all(10.0),
+                        child: Center(
+                          child: Text(
+                            "Chat gestartet am $startDateText",
+                            style: const TextStyle(
+                              color: Colors.grey,
+                              fontSize: 14,
+                              fontStyle: FontStyle.italic,
+                            ),
+                          ),
+                        ),
+                      );
+                    }
+                    final message = messages[index - 1];
+                    return Align(
+                      alignment: message.isSent
+                          ? Alignment.centerRight
+                          : Alignment.centerLeft,
+                      child: IntrinsicWidth(
+                        child: Container(
+                          constraints: BoxConstraints(
+                            maxWidth: MediaQuery.of(context).size.width *
+                                0.6, // Adjusted width
+                          ),
+                          margin: const EdgeInsets.symmetric(
+                              vertical: 5, horizontal: 10),
+                          padding: const EdgeInsets.all(15),
+                          decoration: BoxDecoration(
+                            color: message.isSent
+                                ? Colors.green
+                                : Colors.grey[300],
+                            borderRadius: BorderRadius.only(
+                              topLeft: const Radius.circular(20),
+                              topRight: const Radius.circular(20),
+                              bottomLeft: message.isSent
+                                  ? const Radius.circular(20)
+                                  : Radius.zero,
+                              bottomRight: message.isSent
+                                  ? Radius.zero
+                                  : const Radius.circular(20),
+                            ),
+                          ),
+                          child: Column(
+                            crossAxisAlignment: CrossAxisAlignment.start,
+                            children: [
+                              Text(
+                                message.text,
+                                style: TextStyle(
+                                  color: message.isSent
+                                      ? Colors.white
+                                      : Colors.black,
+                                  fontSize: 16,
+                                ),
+                              ),
+                              const SizedBox(height: 5),
+                              Row(
+                                mainAxisAlignment: MainAxisAlignment.end,
+                                children: [
+                                  Text(
+                                    "${message.timestamp.hour.toString().padLeft(2, '0')}:${message.timestamp.minute.toString().padLeft(2, '0')}",
+                                    style: TextStyle(
+                                      color: message.isSent
+                                          ? Colors.white70
+                                          : Colors.black54,
+                                      fontSize: 12,
+                                    ),
+                                  ),
+                                  if (message.isSent)
+                                    Icon(
+                                      message.isRead
+                                          ? Icons.done_all
+                                          : Icons.check,
+                                      color: message.isRead
+                                          ? Colors.blue
+                                          : Colors.grey,
+                                      size: 16,
+                                    ),
+                                ],
+                              ),
+                            ],
+                          ),
+                        ),
+                      ),
+                    );
                   },
                 );
               },
@@ -136,8 +229,8 @@ class _ChatScreenState extends State<ChatScreen> {
               onPressed: () async {
                 if (_controller.text.isNotEmpty) {
                   setState(() {
-                    messages
-                        .add(Message(_controller.text, true, DateTime.now()));
+                    messages.add(Message(_controller.text, true, DateTime.now(),
+                        isRead: true)); // Set isRead to true for testing
                     _controller.clear();
                   });
                 }
@@ -145,5 +238,28 @@ class _ChatScreenState extends State<ChatScreen> {
         ],
       ),
     );
+  }
+
+  String _getWeekday(int weekday) {
+    const weekdays = ["Mo.", "Di.", "Mi.", "Do.", "Fr.", "Sa.", "So."];
+    return weekdays[weekday - 1];
+  }
+
+  String _getMonth(int month) {
+    const months = [
+      "Jan.",
+      "Feb.",
+      "Mär.",
+      "Apr.",
+      "Mai",
+      "Jun.",
+      "Jul.",
+      "Aug.",
+      "Sep.",
+      "Okt.",
+      "Nov.",
+      "Dez."
+    ];
+    return months[month - 1];
   }
 }
